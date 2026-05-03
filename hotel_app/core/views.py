839f362c58c50ee54b737a58e91fa89bc4ce58a9
Rokurook <ma.requena@duocuc.cl>
2026-05-02 20:10:57 -0400

    renderizar el login dentro del template

--- hotel_app/core/views.py
+++ hotel_app/core/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login_estatico(request):
+    return render(request, 'login.html')
--- hotel_app/hotel_app/urls.py
+++ hotel_app/hotel_app/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from core.views import login_estatico
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path("", login_estatico, name="login"),
 ]
