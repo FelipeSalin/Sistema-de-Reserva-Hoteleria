09b24b8903f17e584e5fc0c632c992bbc4ea2806
Rokurook <ma.requena@duocuc.cl>
2026-05-04 01:10:02 -0400

    modificación views - url para formulario

--- hotel_app/core/views.py
+++ hotel_app/core/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import AuthenticationForm
 
-
 # Create your views here.
 def login_estatico(request):
     return render(request, 'login.html') 
@@ -29,7 +28,8 @@ def detalle_habitacion(request):
 # creamos autenticación para home_cliente 
 def iniciar_sesion(request):
     formulario = AuthenticationForm()
-    contex ={
+    context = {
         "formulario": formulario
     }
-    return render(request, 'autenticacion/home_cliente.html, context')
+
+    return render(request, 'autenticacion/home_cliente.html', context)
