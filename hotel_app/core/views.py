96007a8d1b4baa44af33d36e630b6420e2f7bec1
FelipeSalin <fea.salinas@duocuc.cl>
2026-05-04 15:20:43 -0400

    Ruta a Index realizada, Ruta a Login realizada

--- hotel_app/core/views.py
+++ hotel_app/core/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_estatico(request):
-    return render(request, 'login.html') 
+    return render(request, 'index.html') 
 
 #vista de registro
 def registro(request):
@@ -44,4 +44,4 @@ def home_admin(request):
 
 # creamos autenticación para login
 def iniciar_sesion(request):
-    return render(request, "auteticacion/login.html")
+    return render(request, "autenticacion/login.html")
