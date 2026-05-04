63651a1624779314381bf43ebf0a7f6a825c04a0
FelipeSalin <fea.salinas@duocuc.cl>
2026-05-03 22:22:28 -0400

    Settings Profesor Programación Web

--- hotel_app/hotel_app/settings.py
+++ hotel_app/hotel_app/settings.py
@@ -89,7 +89,8 @@ DATABASES = {
         "PASSWORD": "duoc",
         "TEST": {
             "USER": "default_test",
-            "TBLSPACE": "default_test_tbl_tmp"
+            "TBLSPACE": "default_test_tbls",
+            "TBLSPACE_TMP": "default_test_tbls_tmp",
         }
     }
 }
