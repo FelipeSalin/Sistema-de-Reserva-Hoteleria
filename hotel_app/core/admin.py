1fee8c2694f2a9e3e4ea3bea5d2a8e2b403c2b47
FelipeSalin <fea.salinas@duocuc.cl>
2026-05-06 23:34:40 -0400

    Modelos agregados a admin.py

--- hotel_app/core/admin.py
+++ hotel_app/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Hotel, Habitacion, Cliente, Reserva, MetodoPago, Factura
 # Register your models here.
+
+admin.site.register(Hotel)
+admin.site.register(Habitacion)
+admin.site.register(Cliente)
+admin.site.register(Reserva)
+admin.site.register(MetodoPago)
+admin.site.register(Factura)
